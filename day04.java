56527808bbab17a0a14f645a6b1fbe3525f78413
nikhil003 <nikhil.enabothulaa@gmail.com>
2023-11-15 00:41:44 +0530

    added java files

---
+++ day04.java
@@ -0,0 +1,12 @@
+public class day04 {
+    public static void main(String[] args) {
+     float a= 30;
+     float b=4, c,d;
+     c=a/b;//binary operators
+     d=a%b;
+     System.out.println(c);
+     System.out.println(d);
+     c++; //uniary operators
+     System.out.println(c);
+    }
+}
