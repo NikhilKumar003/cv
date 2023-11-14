3afc5ac778e1d57215b761d2808f84cf46e9d851
nikhil003 <nikhil.enabothulaa@gmail.com>
2023-11-15 00:32:04 +0530

    second files

---
+++ day02.java
@@ -0,0 +1,12 @@
+public class day02 {
+    public static void main(String[] args) {
+        int a=10, b=20, c=30;
+        float d=c*b*a;
+        int 1x=20;
+        int $x=20;
+        int _x=30;
+        
+        System.out.println("mutiple of abc:" + d );
+
+    }
+}
---
+++ day03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day03 {
+    public static void main(String[] args) {
+    int a;
+    System.out.println("enter the value:");
+    Scanner b= new Scanner(System.in);
+    a=b.nextInt();
+    System.out.println("entered valued:"+ a); 
+    }
+}
